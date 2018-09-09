9843e40058d41b74d6cd50d57a60b38793e7f33b
guy raviv <guyraviv1997@gmail.com>
2018-09-09 17:24:27 +0300

    practice

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,35 +1,62 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styles: [`
-  .online {
-      color: white;
-  }`]
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-  serverId = 10;
-  serverName = '';
-  serverStatus = 'offline';
+    serverId = 10;
+    serverName = '';
+    serverStatus = 'offline';
+    details = 'password = tuna';
+    isDisplayDetails = false;
+    toggleDisplayDetailesDescreption;
+    clicksArray = [];
 
-  constructor() {
-      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-  }
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+        this.updateToggleDisplayDetailesDescreption();
+    }
 
-  getServerStatus() {
-    return this.serverStatus;
-  }
+    getServerStatus() {
+        return this.serverStatus;
+    }
 
-  getServerColor() {
-      return this.serverStatus === 'online' ? 'green' : 'red';
-  }
+    getServerColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 
-  getServerName() {
-      return this.serverName;
-  }
+    getServerName() {
+        return this.serverName;
+    }
 
-  setServerName(serverName) {
-      this.serverName = serverName;
-  }
+    setServerName(serverName) {
+        this.serverName = serverName;
+    }
+
+    getDetails() {
+        return this.details;
+    }
+
+    toggleDisplayDetails() {
+        if (this.isDisplayDetails) {
+            this.isDisplayDetails = false;
+            this.clicksArray.push('Undisplay details in ' + Date.now());
+        } else {
+            this.isDisplayDetails = true;
+            this.clicksArray.push('Display details in ' + Date.now());
+        }
+
+        this.updateToggleDisplayDetailesDescreption();
+    }
+
+    updateToggleDisplayDetailesDescreption() {
+        if (this.isDisplayDetails) {
+            this.toggleDisplayDetailesDescreption = 'Undisplay detailes';
+        } else {
+            this.toggleDisplayDetailesDescreption = 'Display detailes';
+        }
+    }
 }
