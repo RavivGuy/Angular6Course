082dac0baf032ff92be32e339f1204fb42c8cca9
guy raviv <guyraviv1997@gmail.com>
2018-09-09 14:10:05 +0300

    directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+  .online {
+      color: white;
+  }`]
 })
 export class ServerComponent {
   serverId = 10;
+  serverName = '';
   serverStatus = 'offline';
+
+  constructor() {
+      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getServerColor() {
+      return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  getServerName() {
+      return this.serverName;
+  }
+
+  setServerName(serverName) {
+      this.serverName = serverName;
   }
 }
